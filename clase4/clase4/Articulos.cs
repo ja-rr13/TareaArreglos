cd6ab4e0a057eb55f4f5f6371dc9d8257a618f63
ja-rr13 <jahirodriguezreyes2006@gmail.com>
2024-10-11 16:50:30 -0600

    TareaArreglos

---
+++ clase4/clase4/Articulos.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace clase4
+{
+    internal class Articulos
+    {
+        public static int[] id = new int[3];
+        public static string[] nombre = new string[3];
+        public static int[] cantidad = new int[3];
+        public static int[] bodega = new int[3];
+        public static void InicializarArreglos()
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                id[i] = 0;
+                nombre[i] = "";
+                
+            }
+            Console.WriteLine("Los Arreglos han sido inicializados");
+        }
+        public static void IngresarProductos()
+        {
+            int indice = 0;
+            while (indice < id.Length)
+            {
+                Console.WriteLine("Digite un codigo");
+                id[indice] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite un articulo");
+                nombre[indice] = Console.ReadLine();
+                Console.WriteLine("Digite el numero de bodega donde se almacena");
+                bodega[indice] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite la cantidad de articulos en la bodega");
+                cantidad[indice] = int.Parse(Console.ReadLine());
+                
+                indice++;
+             
+               
+                
+
+
+            }
+            Console.WriteLine("Los articulos han sido agregados");
+        }
+        public static void ModificarProdructos()
+        {
+            
+            Console.WriteLine("Digite el codigo del articulo que desea modificar: ");
+            int nuevocodigo=int.Parse(Console.ReadLine());
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (nuevocodigo == id[i])
+                {
+                    Console.WriteLine($"Codigo: {id[i]} Nombre: {nombre[i]}");
+                    Console.WriteLine("Digite un nuevo codigo");
+                    id[i] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite un nuevo articulo");
+                    nombre[i] = Console.ReadLine();
+                    Console.WriteLine("Digite el nuevo numero de bodega donde se almacena");
+                    bodega[i] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite la cantidad de articulos en la bodega");
+                    cantidad[i] = int.Parse(Console.ReadLine());
+                } 
+               
+
+            }
+          
+
+        }
+        public static void ConsultarProdructos()
+        {
+            Console.Clear();
+            Console.WriteLine("****Reporte Articulos****");
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.WriteLine($"Codigo: {id[i]} Nombre: {nombre[i]} N.Bodega: {bodega[i]} Cantidad: {cantidad[i]}");
+               
+            }
+            Console.WriteLine("****Fin Del Reporte****"); 
+        }
+        public static void ExcluirProductos()
+        {
+            int br = 0;
+
+            Console.WriteLine("Digite el codigo del articulo que desea borrar");
+            br = int.Parse(Console.ReadLine());
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (br == id[i])
+                {
+                    id[i] = 0;
+                    nombre[i] = "";
+                    bodega[i] = 0;
+                    cantidad[i] = 0;
+                    Console.WriteLine("Se borro el articulo");
+                    break;
+                   
+
+                }
+            }
+        }
+        public static void BuscarArticulo()
+        {
+            int bs = 0;
+            Console.WriteLine("Digite el codigo del articulo que desea buscar");
+            bs = int.Parse(Console.ReadLine());
+            for (int i = 0; i < id.Length; i++)
+            {
+              
+                if (bs.Equals(id[i]))
+                {
+                    Console.WriteLine($"Codigo: {id[i]} Nombre: {nombre[i]} Cantidad: {cantidad[i]}");
+                    break;
+
+
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ clase4/clase4/menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase4
+{
+    internal class menu
+    {
+        public static void principal()
+        {
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1.Inicializar arreglos");
+                Console.WriteLine("2.Ingresar articulos");
+                Console.WriteLine("3.Modificar Articulos");
+                Console.WriteLine("4.Borrar articulos");
+                Console.WriteLine("5.Consultar articulos");
+                Console.WriteLine("6.Buscar articulo por codigo");
+                Console.WriteLine("7.Salir");
+                Console.WriteLine("Digite una opcion");
+                opcion = int.Parse(Console.ReadLine()); 
+                switch (opcion)
+                {
+                    case 1:Articulos.InicializarArreglos(); break;
+                    case 2:Articulos.IngresarProductos(); break;
+                    case 3:Articulos.ModificarProdructos(); break;
+                    case 4:Articulos.ExcluirProductos(); break;
+                    case 5:Articulos.ConsultarProdructos(); break;
+                    case 6:Articulos.BuscarArticulo(); break;
+                    case 7: break;  
+
+                    default: Console.WriteLine("***Opcion incorrecta***"); break;
+                }
+
+            } while (opcion!=7);
+        }
+    }
+}
